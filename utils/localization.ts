74ba32cf5a90f537a959eb89f52f90b4bc58b4c8
Camilo Montoya <camilo.montoya@tuix.ch>
2023-08-30 11:13:33 -0500

    feat(): fix build

--- utils/localization.ts
+++ utils/localization.ts
@@ -3,19 +3,19 @@ import langParser from 'accept-language-parser';
 
 export const getRequestedLocales = (request: NextRequest) => {
   const requestedLocales = request.headers.get('accept-language');
-  const parsed = langParser.parse(requestedLocales);
+  const parsed = langParser.parse(requestedLocales || '');
   const locales = parsed.map(
     (locale) => `${locale.code}${locale.region ? `-${locale.region}` : ''}`
   );
   return locales;
 };
 
-export const getLocaleInPathname = (pathname) => {
+export const getLocaleInPathname = (pathname: string) => {
   const pathnameParts = pathname.toLowerCase().split('/');
   return pathnameParts[1];
 };
 
-export const isPathnameMissingValidLocale = (locales: string[], pathname) =>
+export const isPathnameMissingValidLocale = (locales: string[], pathname: string) =>
   locales.every((locale) => {
     return !pathname.startsWith(`/${locale}`);
   });
